d185099509c69af4c0867598a93e0e885bdd0551
tanynova69 <90447391+tanynova99@users.noreply.github.com>
2024-04-12 12:04:21 +0000

    rand num

--- name_gen.py
+++ name_gen.py
@@ -1,7 +1,7 @@
 import random
 import streamlit as st
 
-st.title("Geospatial Name Generator:")
+st.title("Let's generate your variant number:")
 
 #names that fit in the first word
 first = ('geo','digital','quantum','spatial','terra','flight','imagery','sciences','environmental','research','one','air','maps','blue','sky','carto','labs','civic','eco','hub','site','map','box','max','open','data','survey','pic','sat','space','voxel','world','location','intelligence','space','intelli','plex','GIS','sensing','systems','agro','shape','analytics','green','motion','point cloud','global','planet','hydro','evo','aerial','carta','carto','AI','3D','4D','metaverse','super','climate','land','topo','spectrum','white','black','raster','co-op','vision','cloud','scape','agro')
@@ -11,21 +11,8 @@ last = ('geo','digital','quantum','spatial','terra','flight','imagery','sciences
 endcap = ('Systems', 'Labs', 'group', 'institute', 'analytics')
 
 if st.button("Click me!"):
-    for i in range(3):
-        k = random.randint(0,4)
-        if k != 4:
-            first_name = random.choice(first)
-            last_name = random.choice(last)
-            while (True):
-                if (first_name != last_name):
-                    break  
-                else: 
-                    first_name = random.choice(first)
-                    last_name = random.choice(last)  
-            group=''.join(first_name + ' ' + last_name)
-        else: 
-            group=''.join(random.choice(first)+" "+ random.choice(last) + " "+ random.choice(endcap))
-        st.write(group.title())
+        k = random.randint(1,15)
+        st.write(k)
 else:
     st.write("Waiting for you to click the button")
     
